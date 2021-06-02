b7ae0f76a3d8cfd45b862dbad8bc65c528d9c282
Yeshwanth KM <71647983+YeshwanthKM@users.noreply.github.com>
2021-06-02 11:35:28 +0530

    Update game.js

--- game.js
+++ game.js
@@ -39,7 +39,7 @@ var startGame = function() {
     Game.setBoard(1,new Starfield(50,0.6,100));
     Game.setBoard(2,new Starfield(100,1.0,50));
   }  
-  Game.setBoard(3,new TitleScreen("Yesh Alien Shooter", 
+  Game.setBoard(3,new TitleScreen("Yesh Alien Fight", 
                                   "Press space to start playing",
                                   playGame));
 };
